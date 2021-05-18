e6673a982c84e9cf889caa00d8bb5870e1e550f2
jennawriteswords <83444930+jennawriteswords@users.noreply.github.com>
2021-05-18 13:21:08 -0400

    Updated the time

--- src/scripts.js
+++ src/scripts.js
@@ -1,15 +1,39 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hour = date.getHours();
+  if (hour < 10) {
+    hour = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hour}:${minutes}`;
+}
+
 function displayTemperature(response) {
-  console.log(response.data);
   let temperature = document.querySelector("#temperature");
   let city = document.querySelector("h1");
   let conditions = document.querySelector("#conditions");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   temperature.innerHTML = Math.round(response.data.main.temp);
   city.innerHTML = response.data.name;
   conditions.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = "Humidity: " + response.data.main.humidity + "%";
   wind.innerHTML = "Wind: " + Math.round(response.data.wind.speed) + " mph";
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let city = "New York";
