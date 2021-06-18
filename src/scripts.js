b0770dd5ed56d1c87886f7727ba9a6b5e06f8bd2
jennawriteswords <83444930+jennawriteswords@users.noreply.github.com>
2021-06-17 22:27:02 -0400

    Connected API to find coordiantes

--- src/scripts.js
+++ src/scripts.js
@@ -46,6 +46,12 @@ function displayForecast() {
   forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+  let apiKey = "b81214d7dc51d729ec2db083181120c3";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
   let temperature = document.querySelector("#temperature");
   let conditions = document.querySelector("#conditions");
